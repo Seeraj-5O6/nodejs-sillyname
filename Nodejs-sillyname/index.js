40be4a030cf82b290539d7903a4c30e705444db7
Seeraj-5O6 <78401605+Seeraj-5O6@users.noreply.github.com>
2024-05-20 08:18:31 +0530

    "Sillyname using nodejs "

---
+++ Nodejs-sillyname/index.js
@@ -0,0 +1,11 @@
+
+
+
+//=========================================================================
+//var generateName = require('sillyname'); 1 
+              //or                                // we use either 1 or 2 , every use max import 
+  import generateName from "sillyname"; //2
+
+
+var sillyName = generateName();
+console.log(`My sillyname is ${sillyName}`);
